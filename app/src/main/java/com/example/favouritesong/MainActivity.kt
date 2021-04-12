cfd69ce2307d86eb0858e4ee772ad0079088197c
AgaKing02 <agoohanpeyishbek@gmail.com>
2021-04-12 22:25:39 +0600

    Initial Commit

--- app/src/main/java/com/example/favouritesong/MainActivity.kt
+++ app/src/main/java/com/example/favouritesong/MainActivity.kt
@@ -24,6 +24,8 @@ class MainActivity : AppCompatActivity() {
         var counter = 0;
 
         button.setOnClickListener {
+            if(counter==0)
+                textView.text=""
             try {
                 textView.append(lyrics.get(counter) + "\n")
             } catch (e: Exception) {
